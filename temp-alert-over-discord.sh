ed35e2febdcc99a4f58d0a4367c208413c2d1ecf
TheAlphaCeph <73220426+TheAlphaCeph@users.noreply.github.com>
2022-05-19 12:56:00 +0000

    Create temp-alert-over-discord.sh

---
+++ temp-alert-over-discord.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Discord webhook URL
+url=https://discord.com/api/webhooks/XXXXXXXX/XXXXXXXXX
+
+# Temperature threshold for when to send alert
+threshold=80
+
+sensors | grep -e "temp1" | while read line; do
+        temp=$(echo $line | awk -F "+" '{ print $2 }' | awk -F "." '{ print $1 }');
+        # Alert message:
+        message="ALERT for $(hostname). Temperature is $temp degrees"
+        if (( temp > $threshold )); then
+                curl -H 'Content-type: application/json' -X POST -d "{\"content\":\"$message\"}" $url;
+        fi;
+        done
