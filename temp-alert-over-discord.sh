b89b667c1b170871929ca09673edf32c211ea08e
Zerodya <73220426+Zerodya@users.noreply.github.com>
2022-06-27 08:12:27 +0000

    Update temp-alert-over-discord.sh

--- temp-alert-over-discord.sh
+++ temp-alert-over-discord.sh
@@ -8,8 +8,10 @@ threshold=80
 
 sensors | grep -e "temp1" | while read line; do
         temp=$(echo $line | awk -F "+" '{ print $2 }' | awk -F "." '{ print $1 }');
+        
         # Alert message:
-        message="ALERT for $(hostname). Temperature is $temp degrees"
+        message="ALERT for $(hostname). Temperature is $temp degrees. That's hot!"
+        
         if (( temp > $threshold )); then
                 curl -H 'Content-type: application/json' -X POST -d "{\"content\":\"$message\"}" $url;
         fi;
