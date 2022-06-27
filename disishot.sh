3688d91b1a1725dbf5c8c4e9fbb1515730d9afef
Zerodya <73220426+Zerodya@users.noreply.github.com>
2022-06-27 17:06:45 +0000

    Update disishot.sh

--- disishot.sh
+++ disishot.sh
@@ -82,13 +82,13 @@ fi
 # Executes the actual script
 sensors | grep -e "$tempsensor" | while read line; do
 	# Get temp
-        temp=$(echo $line | awk -F "+" '{ print $2 }' | awk -F "." '{ print $1 }');
+	temp=$(echo $line | awk -F "+" '{ print $2 }' | awk -F "." '{ print $1 }');
 	
 	# Alert message:
-        message="ALERT for $(hostname). Temperature is $temp degrees."
-
+	message="ALERT for $(hostname). Temperature is $temp degrees."
+	
 	# Send Discord notification
-        if (( temp > $threshold )); then
+	if (( temp > $threshold )); then
                 curl -H 'Content-type: application/json' -X POST -d "{\"content\":\"$message\"}" $url;
-        fi;
-        done
+	fi;
+done
