67e1c2ac2dc7c3ad8f1372738cc6fc856273ecb1
Zerodya <73220426+Zerodya@users.noreply.github.com>
2022-06-27 11:08:43 +0000

    Update disisfine.sh

--- disisfine.sh
+++ disisfine.sh
@@ -4,7 +4,7 @@
 url=https://discord.com/api/webhooks/XXXXXXXX/XXXXXXXXX
 
 # Which temperature to monitor (Run `sensors` to find which one you want to monitor)
-temptype="temp1"
+tempsensor="temp1"
 
 sensors | grep -e "$temptype" | while read line; do
         temp=$(echo $line | awk -F "+" '{ print $2 }' | awk -F "." '{ print $1 }');
